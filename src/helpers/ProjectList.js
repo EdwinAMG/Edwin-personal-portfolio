3927112e2f0f4e1abe7600bb7859c6c3d8785dfa
Edwin M <andresedwinmg14@gmail.com>
2023-09-29 00:31:28 -0500

    CSS de home completado

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,18 +1,26 @@
 import project1 from "../assets/project-1.jpg";
 import project2 from "../assets/project-2.jfif";
+import project3 from "../assets/project-3.png";
 
 export const ProjectList = [
   {
+    name: "Personal Portfolio",
+    image: project3,
+    skills: "ReactJS, CSS, Firebase,JavaScript",
+    gitHub: "https://github.com/EdwinAMG/Edwin-personal-portfolio",
+    web: "portfolio-edwinmonsalve.web.app/",
+  },
+  {
     name: "Expense Tracker",
     image: project1,
-    skills: "ReactJS, CSS, Firebase",
+    skills: "ReactJS, CSS, Firebase, JavaScript",
     gitHub: "https://github.com/EdwinAMG/expenses-tracker",
     web: "expense-tracker-81e43.web.app",
   },
   {
     name: "List of all Pokemons using Pokemon API",
     image: project2,
-    skills: "ReactJS, CSS, API",
+    skills: "ReactJS, CSS, JavaScript, API",
     gitHub: "https://github.com/EdwinAMG/Pokemon-Web",
     web: "edwinamg.github.io/Pokemon-Web/",
   },
